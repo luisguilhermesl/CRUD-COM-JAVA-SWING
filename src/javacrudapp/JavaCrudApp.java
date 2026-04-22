79bd07ae48702a51b40fd3ca74e4c415f434d6d4
Luís Guilherme de S Lopes <luisgslopes2@gmail.com>
2026-04-22 16:29:52 -0300

    22.04.26

---
+++ src/javacrudapp/JavaCrudApp.java
@@ -0,0 +1,17 @@
+package javacrudapp;
+
+/**
+ *
+ * @author LUISGUILHERMEDESOUZA
+ */
+public class JavaCrudApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // new CrudAppFrame().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
